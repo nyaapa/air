be766ef4a3d049309fd5c71db098d4d6c9574815
nyaapa <nyaapa@gmail.com>
2021-03-27 06:16:31 +0000

    format

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@
 #include <exception>
 #include <iostream>
 #include <optional>
+#include <string>
 #include <thread>
 #include <type_traits>
-#include <string>
 
 namespace {
 void init_handler(auto& h) {
@@ -69,12 +69,9 @@ int main(int argc, char** argv) {
 	std::string name;
 	std::string receiver;
 
-	options.add_options()
-		("s,sleep-time", "sleep time between probes", cxxopts::value<uint>(sleep_time))
-		("j,json", "response in json", cxxopts::value<bool>(json))
-		("n,name", "name of that sender, required for sending", cxxopts::value<std::string>(name))
-		("r,receiver", "receiver address, requires name", cxxopts::value<std::string>(receiver))
-		("h,help", "Print help");
+	options.add_options()("s,sleep-time", "sleep time between probes", cxxopts::value<uint>(sleep_time))(
+	    "j,json", "response in json", cxxopts::value<bool>(json))("n,name", "name of that sender, required for sending", cxxopts::value<std::string>(name))(
+	    "r,receiver", "receiver address, requires name", cxxopts::value<std::string>(receiver))("h,help", "Print help");
 
 	auto result = options.parse(argc, argv);
 
@@ -88,7 +85,7 @@ int main(int argc, char** argv) {
 		exit(0);
 	}
 
-	if (std::find_if(name.begin(), name.end(), [](char c){return !isalnum(c);}) != name.end()) {
+	if (std::find_if(name.begin(), name.end(), [](char c) { return !isalnum(c); }) != name.end()) {
 		fmt::print("Name should be only alphanumerical {}\n", options.help({""}));
 		exit(0);
 	}
@@ -111,9 +108,7 @@ int main(int argc, char** argv) {
 		if (json) {
 			std::string result{fmt::format("\"name\":\"{}\"", name)};
 			add_data(s8h, [&result](const auto& data) mutable { result += fmt::format(",\"co2\":{}", data.co2); });
-			add_data(sds011h, [&result](const auto& data) mutable {
-				result += fmt::format(",\"deca_pm25\":{},\"deca_pm10\":{}", data.deca_pm25, data.deca_pm10);
-			});
+			add_data(sds011h, [&result](const auto& data) mutable { result += fmt::format(",\"deca_pm25\":{},\"deca_pm10\":{}", data.deca_pm25, data.deca_pm10); });
 			add_data(bme280h, [&result](const auto& data) mutable {
 				result += fmt::format(",\"deca_humidity\":{},\"deca_kelvin\":{}", data.deca_humidity, data.deca_kelvin);
 			});
